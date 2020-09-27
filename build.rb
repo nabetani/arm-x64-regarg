ee838478ed9a744e4a5ed29ad5acba5279e7c39c
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-09-27 14:12:32 +0900

    スレッド使ってビルドを速くする

--- build.rb
+++ build.rb
@@ -1,6 +1,5 @@
 require "fileutils"
-
-SRC_FN = "hoge.c"
+require "securerandom"
 
 def dest_fn(cpu, name)
   "asm/#{cpu}_#{name}.s"
@@ -48,8 +47,44 @@ Args = Struct.new( :arg_list ) do
   end
 end
 
+def create_asms(a)
+  fn = "#{SecureRandom.uuid}.c"
+  File.open( fn, "w" ) do |f|
+    f.puts <<~SRC
+      enum{ assert_short_is_2bytes = 1/((sizeof(short)==2)?1:0) };
+      enum{ assert_int_is_4bytes = 1/((sizeof(int)==4)?1:0) };
+      enum{ assert_long_long_is_8bytes = 1/((sizeof(long long)==8)?1:0) };
+      struct short1{ short a; };
+      typedef struct short1 short1;
+      struct char2{ char a, b; };
+      typedef struct char2 char2;
+      struct int2{ int a, b; };
+      typedef struct int2 int2;
+      struct int4{ int a, b, c, d; };
+      typedef struct int4 int4;
+      struct int_f{ int a; float b; };
+      typedef struct int_f int_f;
+      struct int_d{ int a; double b; };
+      typedef struct int_d int_d;
+      void caller_function (){
+        extern void callee_function( #{a.decls}  );
+        callee_function( #{a.values} );
+      }
+    SRC
+  end
+  %x( aarch64-none-elf-gcc -O2 #{fn} -mabi=ilp32 -mcpu=cortex-a53 -march=armv8-a -S -o #{dest_fn("cortex-a53-ilp32", a.name)} )
+  %x( aarch64-none-elf-gcc -O2 #{fn} -mabi=lp64 -mcpu=cortex-a53 -march=armv8-a -S -o #{dest_fn("cortex-a53-lp64", a.name)} )
+  %x( arm-none-eabi-gcc -O2 #{fn} -mcpu=cortex-a53 -mfloat-abi=hard -S -o #{dest_fn("cortex-a53-32hard", a.name)} )
+  %x( arm-none-eabi-gcc -O2 #{fn} -mcpu=cortex-a53 -mfloat-abi=soft -S -o #{dest_fn("cortex-a53-32soft", a.name)} )
+  %x( arm-none-eabi-gcc -O2 #{fn} -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -S -o #{dest_fn("cortex-m4-hard", a.name)} )
+  %x( arm-none-eabi-gcc -O2 #{fn} -mthumb -mcpu=cortex-m4 -mfloat-abi=soft -mfpu=fpv4-sp-d16 -S -o #{dest_fn("cortex-m4-soft", a.name)} )
+  %x( gcc-10 -mabi=ms -O2 #{fn} -S -o #{dest_fn("x64ms", a.name)} )
+  %x( gcc-10 -mabi=sysv -O2 #{fn} -S -o #{dest_fn("x64sysv", a.name)} )
+  FileUtils.rm(fn)
+end
+
 [*1..17].each do |i|
-  [
+  threads = [
     Args.new( "g"*i ),
     Args.new( "h"*i ),
     Args.new( "j"*i ),
@@ -63,37 +98,10 @@ end
     Args.new( "f"*i ),
     Args.new( "d"*i ),
     Args.new( "p"*i ),
-  ].each do |a|
-    File.open( SRC_FN, "w" ) do |f|
-      f.puts <<~SRC
-        enum{ assert_short_is_2bytes = 1/((sizeof(short)==2)?1:0) };
-        enum{ assert_int_is_4bytes = 1/((sizeof(int)==4)?1:0) };
-        enum{ assert_long_long_is_8bytes = 1/((sizeof(long long)==8)?1:0) };
-        struct short1{ short a; };
-        typedef struct short1 short1;
-        struct char2{ char a, b; };
-        typedef struct char2 char2;
-        struct int2{ int a, b; };
-        typedef struct int2 int2;
-        struct int4{ int a, b, c, d; };
-        typedef struct int4 int4;
-        struct int_f{ int a; float b; };
-        typedef struct int_f int_f;
-        struct int_d{ int a; double b; };
-        typedef struct int_d int_d;
-        void caller_function (){
-          extern void callee_function( #{a.decls}  );
-          callee_function( #{a.values} );
-        }
-      SRC
+  ].map{ |a|
+    Thread.new do 
+      create_asms(a)
     end
-    %x( aarch64-none-elf-gcc -O2 #{SRC_FN} -mabi=ilp32 -mcpu=cortex-a53 -march=armv8-a -S -o #{dest_fn("cortex-a53-ilp32", a.name)} )
-    %x( aarch64-none-elf-gcc -O2 #{SRC_FN} -mabi=lp64 -mcpu=cortex-a53 -march=armv8-a -S -o #{dest_fn("cortex-a53-lp64", a.name)} )
-    %x( arm-none-eabi-gcc -O2 #{SRC_FN} -mcpu=cortex-a53 -mfloat-abi=hard -S -o #{dest_fn("cortex-a53-32hard", a.name)} )
-    %x( arm-none-eabi-gcc -O2 #{SRC_FN} -mcpu=cortex-a53 -mfloat-abi=soft -S -o #{dest_fn("cortex-a53-32soft", a.name)} )
-    %x( arm-none-eabi-gcc -O2 #{SRC_FN} -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -S -o #{dest_fn("cortex-m4-hard", a.name)} )
-    %x( arm-none-eabi-gcc -O2 #{SRC_FN} -mthumb -mcpu=cortex-m4 -mfloat-abi=soft -mfpu=fpv4-sp-d16 -S -o #{dest_fn("cortex-m4-soft", a.name)} )
-    %x( gcc-10 -mabi=ms -O2 #{SRC_FN} -S -o #{dest_fn("x64ms", a.name)} )
-    %x( gcc-10 -mabi=sysv -O2 #{SRC_FN} -S -o #{dest_fn("x64sysv", a.name)} )
-  end
+  }
+  threads.each(&:join)
 end
