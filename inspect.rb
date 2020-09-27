5cee131cf5d6b0292ccf250df64211e1c4626c18
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-09-27 21:29:58 +0900

    複数の型を使う

--- inspect.rb
+++ inspect.rb
@@ -20,8 +20,8 @@ end
 def single_conds
   names = Dir.glob( "asm/*.s" ).map{ |e|
     m=%r!asm/(.*)_((\w)\3*)\.s$!.match(e)
-    [m[1],m[2][0],m[2].size]
-  }
+    m && [m[1],m[2][0],m[2].size]
+  }.compact
   sorters = [
     ->(v){[is_name(v),v]},
     ->(v){"bsilpfdhgjkxy".index(v)},
@@ -29,6 +29,22 @@ def single_conds
   ]
   Array.new(3){ |ix|
     names.map{ |e| e[ix] }.uniq.sort_by{ |v| sorters[ix][v] }
+  }
+end
+
+def multi_conds
+  names = Dir.glob( "asm/*.s" ).map{ |e|
+    m=%r!asm/(.*)_(\w+)\.s$!.match(e)
+    target = m[1]
+    types = m[2]
+    if types[0]!=types[-1]
+      [target,types]
+    else
+      nil
+    end
+  }.compact
+  Array.new(2){ |ix|
+    names.map{ |e| e[ix] }.uniq.sort
   }
 end
 
@@ -150,11 +166,55 @@ def single(targets, types, counts)
   end
 end
 
-def main
+def single_main
   targets, types, counts = single_conds
   etypes = "bsilfdp".chars
+  puts( "\## 一種類の型\n\n")
+  puts( "\n### 組み込み型\n\n")
   single( targets, types & etypes, counts )
+  puts( "\n### 構造体\n\n")
   single( targets, types-etypes, counts )
 end
 
-main
+def multi(target, t)
+  tis = [0, -1].map{ |ix|
+    [t[0][ix], t.map{ |e| e.count(e[ix]) }.max]
+  }
+  col_count = tis[1][1]
+  tlist = tis.map{|tt,|dispname(tt)}.join("＼")
+  puts <<~"HEAD"
+
+    #### #{target}
+
+    |#{tlist}|#{[*1..col_count].join("|")}|
+    |:--|#{([":-:"]*col_count).join("|")}|
+  HEAD
+  prev_all_false=false
+  (1..tis[0][1]).each do |l0|
+    line = (1..tis[1][1]).map{ |l1|
+      txt = tis[0][0]*l0 + tis[1][0]*l1
+      no_stack?(target, "asm/#{target}_#{txt}.s")
+    }
+    break if prev_all_false && line.none?
+    prev_all_false = line.none?
+    cols=line.map{ |e| e ? "✅" : "❌" }.join("|")
+    puts("|#{l0}|#{cols}|")
+  end
+end
+
+def multi_main
+  puts( "\n## 複数の型の混在\n\n")
+  targets, types = multi_conds
+  t0s = types.map{ |e| e.chars.uniq.sort.join }.sort.uniq
+  t0s.each do |t0|
+    t = types.select{ |e| (e.chars.uniq - t0.chars).empty? }.sort
+    tlist = [t[0][0], t[-1][-1]].map{|e| dispname(e)}.join("＼")
+    puts( "\n### #{tlist}\n\n")
+    targets.each do |target|
+      multi(target,t)
+    end
+  end
+end
+
+single_main
+multi_main
